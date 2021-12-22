ccaaeadc5c86c780058569dca625792ad4e4e1c5
zzq <>
2021-12-22 19:54:37 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import Tree from './components/Tree.vue'
+import { ref} from "vue"
+let nodeData = ref({
+    id: 1,
+    clickCount: 1,
+    children:[
+        {
+            id: 2,
+            clickCount: 2,
+            children: [
+                {
+                    id: 3,
+                    clickCount: 3,
+                    children: []
+                }
+            ]
+        },
+        {
+            id: 5,
+            clickCount: 2,
+            children: [
+                {
+                    id: 6,
+                    clickCount: 3,
+                    children: []
+                }
+            ]
+        }
+    ]
+})
+</script>
+
+<template>
+  <Tree :list="[nodeData]"></Tree>
+</template>
+
+<style>
+#app {
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Tree.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="tree-body">                                    
+    <li v-for="item in list" :id="item.id">
+      <div>
+        id:{{item.id}}
+        count: {{item.clickCount}}
+        <button @click="add(item)">+1</button>
+      </div>      
+      <tree :list="item.children"></tree> 
+    </li>
+  </ul>
+</template>
+
+<script setup>
+defineProps({
+  list: Array
+})
+
+function add(item){
+  ++item.clickCount
+}
+</script>
+
+<style>
+.tree-body{
+  margin: 10px 30px 0;
+}
+</style>
